cb971568791d9581c1f5acf7cb39bf0b84ebeaeb
Karl Hallsby <karl@hallsby.com>
2023-01-21 16:10:04 -0600

    Include a gloa-main function to be used in regular usage
    gloa.scm: Add gloa-main function to act as front-end for command-line usage.

--- gloa.scm
+++ gloa.scm
@@ -1,4 +1,10 @@
 (define-module (gloa)
-  #:use-module (gloa hello))
+  #:use-module (gloa hello)
+  #:export (gloa-main))
 
-(hello-world)
+(define* (gloa-main arg0 . args)
+  ;; Add gloa site directory to Guile's load path so that user's can
+  ;; easily import their own modules.
+  (add-to-load-path (getcwd))
+  (setlocale LC_ALL "")
+  (hello-world))
