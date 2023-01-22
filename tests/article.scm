d0210572d2aded9d8e8a68743a09bc076e64cea5
Karl Hallsby <karl@hallsby.com>
2023-01-21 18:37:00 -0600

    Define common article test information to be reused through all tests

--- tests/article.scm
+++ tests/article.scm
@@ -2,6 +2,14 @@
   #:use-module (gloa article)
   #:use-module (srfi srfi-64))
 
+(define %test-title "Test title")
+(define %test-authors
+  '("FirstName1 LastName1"
+    "FirstName2 MI. LastName2"))
+
+(define %test-article
+  (make-article %test-title %test-authors))
+
 (test-begin "article-creation")
 
 (test-assert "create article"
