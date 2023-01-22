56f7c915601b89f56aaf87848217f620c1165ad4
Karl Hallsby <karl@hallsby.com>
2023-01-21 18:31:41 -0600

    Rename article creation test collection to article-creation
    * tests/article.scm: article-creation is a more descriptive name of the
    intended behavior this set of tests is supposed to have.

--- tests/article.scm
+++ tests/article.scm
@@ -2,11 +2,11 @@
   #:use-module (gloa article)
   #:use-module (srfi srfi-64))
 
-(test-begin "article-info")
+(test-begin "article-creation")
 
 (test-assert "create article"
   (make-article "Test title"
                 '("FirstName1 LastName1"
                   "FirstName2 LastName2")))
 
-(test-end "article-info")
+(test-end "article-creation")
