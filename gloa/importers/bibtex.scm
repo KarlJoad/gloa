3cc8411923974a110ac983d6973f997e1af27dac
Karl Hallsby <karl@hallsby.com>
2023-02-04 20:28:56 -0600

    Parse BibTeX citation body's tags into an alist

--- gloa/importers/bibtex.scm
+++ gloa/importers/bibtex.scm
@@ -10,11 +10,23 @@ returned."
 
 (define (parse-bibtex file-port)
   "Parse an opened file into an alist."
+  (define trimming-character-set (char-set-union char-set:whitespace char-set:punctuation))
+  (define (split-tag-string s) (string-split s #\=))
+  (define (create-tag-pair tag-as-string)
+    (cons (string->symbol (car tag-as-string))
+          (string-trim (string-trim (cadr tag-as-string) trimming-character-set))))
   (with-input-from-port file-port
     (lambda ()
-      (let ((article-type (read-delimited "{"))
-            (article-id   (read-line))
-            (tags-list    (read-bibtex-body)))))))
+      ;; Use let* because reading must be properly sequenced
+      (let* ((article-type (string-trim-both (read-delimited "{") trimming-character-set))
+             (article-id   (string-trim-both (read-line) trimming-character-set))
+             (tags-list    (read-bibtex-body)))
+        ;; Convert list of tags to alist. Map tag -> entry
+        `((type . ,article-type)
+          (id . ,article-id)
+          ,@(map (lambda (s) (create-tag-pair (split-tag-string s)))
+             ;; Remove leading/trailing whitespace and trailing punctuation
+             (map (lambda (s) (string-trim-both s trimming-character-set)) tags-list)))))))
 
 (define (read-bibtex-body)
   "Read the body of tags for a BibTeX entry, returning a list of strings.
