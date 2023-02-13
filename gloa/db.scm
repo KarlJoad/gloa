d40e4d2d560ba5dc601c8347054f0603185a8fdf
Karl Hallsby <karl@hallsby.com>
2023-02-12 19:07:54 -0600

    Add procedure to close a SQLite3 database connection

--- gloa/db.scm
+++ gloa/db.scm
@@ -1,9 +1,14 @@
 (define-module (gloa db)
   #:use-module (sqlite3)
-  #:export (open-db))
+  #:export (open-db
+            close-db))
 
 (define (open-db db-path)
   "Open the SQLite3 database at the provided DB-PATH.
 If the database has not been created at DB-PATH, an error is raised.
 Requires that db-path actually be an SQLite3 database."
   (sqlite-open db-path (logior SQLITE_OPEN_READWRITE)))
+
+(define (close-db db)
+  "Close the provided DB connection."
+  (sqlite-close db))
