521d629d956750b79db9061e00d7af8dabb34e2a
Karl Hallsby <karl@hallsby.com>
2023-02-14 11:34:49 -0600

    Define the database path to use for database unit tests

--- tests/db.scm
+++ tests/db.scm
@@ -3,6 +3,8 @@
   #:use-module (srfi srfi-64)
   #:use-module (tests utils))
 
+(define %testing-database-path "test.db")
+
 (with-tests "db-tests"
   (test-error "open-missing-db" 'sqlite-error
     (open-db "not-present.db")))
