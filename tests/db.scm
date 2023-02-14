2d0d0561066b4d69b60291acac870fee6c6af7c6
Karl Hallsby <karl@hallsby.com>
2023-02-14 11:35:05 -0600

    Define a cleanup procedure for unti tests that use a database

--- tests/db.scm
+++ tests/db.scm
@@ -5,6 +5,14 @@
 
 (define %testing-database-path "test.db")
 
+(define (cleanup-test-db db)
+  "Close DB and remove its file as cleanup when done using the testing database.
+If the database pointer passed in DB is already closed, this procedure runs without
+exception, deleting the database file.
+If the file does not exist, then an exception is raised."
+  (close-db db)
+  (delete-file %testing-database-path))
+
 (with-tests "db-tests"
   (test-error "open-missing-db" 'sqlite-error
     (open-db "not-present.db")))
