db46d9e4caf71c891b3d44862e87b030a7294200
Karl Hallsby <karl@hallsby.com>
2023-03-28 23:47:03 -0500

    Update BibTeX test to use list of strings for keywords

--- tests/import-bibtex.scm
+++ tests/import-bibtex.scm
@@ -13,7 +13,7 @@
     (month . "Feb")
     (ISSN . "1937-4151")
     (doi . "10.1109/TCAD.2006.884574")
-    (keywords . "")
+    (keywords . (""))
     (abstract . "This paper presents experimental measurements of the differences between a 90-nm CMOS field programmable gate array (FPGA) and 90-nm CMOS standard-cell application-specific integrated circuits (ASICs) in terms of logic density, circuit speed, and power consumption for core logic. We are motivated to make these measurements to enable system designers to make better informed choices between these two media and to give insight to FPGA makers on the deficiencies to attack and, thereby, improve FPGAs. We describe the methodology by which the measurements were obtained and show that, for circuits containing only look-up table-based logic and flip-flops, the ratio of silicon area required to implement them in FPGAs and ASICs is on average 35. Modern FPGAs also contain \"hard\" blocks such as multiplier/accumulators and block memories. We find that these blocks reduce this average area gap significantly to as little as 18 for our benchmarks, and we estimate that extensive use of these hard blocks could potentially lower the gap to below five. The ratio of critical-path delay, from FPGA to ASIC, is roughly three to four with less influence from block memory and hard multipliers. The dynamic power consumption ratio is approximately 14 times and, with hard blocks, this gap generally becomes smaller")
     (pages . "203-215")
     (number . 2)
