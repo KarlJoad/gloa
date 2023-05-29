65342da0546a7adea838d914cc50f45edfb094f6
Karl Hallsby <karl@hallsby.com>
2023-05-29 07:05:49 -0500

    Upon starting gloa, start a REPL
    This takes some inspiration from how Guix starts a REPL when using the
    guix-repl command. In fact, when you invoke "guix repl", you call the
    guix-repl command with a guile-type REPL with no specification. This is
    because you are just running a Guile REPL inside your program.
    
    If you want more advanced features (remote REPLs using sockets), that is NOT
    supported with Gloa.

--- gloa.scm
+++ gloa.scm
@@ -1,10 +1,31 @@
 (define-module (gloa)
   #:use-module (gloa hello)
+  #:use-module (system repl repl)
+  #:declarative? #f
   #:export (gloa-main))
+
+(define user-config
+  ;; The user's personal Guile REPL configuration.
+  (and=> (getenv "HOME")
+         (lambda (home)
+           (string-append home "/.guile"))))
+
+(define (set-user-module)
+  (when (and ;; (not (assoc-ref opts 'ignore-dot-guile?)) ;; TODO: CLI flag for ignoring .guile
+             user-config
+             (file-exists? user-config))
+    (load user-config)))
 
 (define* (gloa-main arg0 . args)
   ;; Add gloa site directory to Guile's load path so that user's can
   ;; easily import their own modules.
   (add-to-load-path (getcwd))
   (setlocale LC_ALL "")
-  (hello-world))
+  (hello-world)
+  (save-module-excursion
+   (lambda ()
+     (set-user-module)
+     ;; Do not exit repl on SIGINT.
+     ((@@ (ice-9 top-repl) call-with-sigint)
+      (lambda ()
+        (start-repl))))))
