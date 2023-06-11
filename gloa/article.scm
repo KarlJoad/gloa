1e253c5e30c1a5400760f944505af9cfbeba6160
Karl Hallsby <karl@hallsby.com>
2023-06-11 07:58:03 -0500

    Add function to deserialize article author string to list

--- gloa/article.scm
+++ gloa/article.scm
@@ -1,11 +1,13 @@
 (define-module (gloa article)
+  #:use-module (gloa utils)
   #:use-module (srfi srfi-9) ; Records
   #:export (article
             make-article
             article?
             article-title
             article-authors
-            serialize-article-authors))
+            serialize-article-authors
+            deserialize-article-authors))
 
 (define-record-type <article>
   (make-article title authors)
@@ -15,3 +17,6 @@
 
 (define (serialize-article-authors author-list)
   (string-join author-list " and "))
+
+(define (deserialize-article-authors author-string)
+  (string-split-substring author-string " and "))
