df842876baa5a0e27adc559d957a8b5944939448
Karl Hallsby <karl@hallsby.com>
2023-06-11 07:53:27 -0500

    Simplify article-present predicate by searching the database
    The SQL queries for searching the database and determining if an article was
    present were identical anyways. The only difference now is that we are
    counting the number of article objects in the list, rather than the number of
    vectors in the list.

--- gloa/db/utils.scm
+++ gloa/db/utils.scm
@@ -1,19 +1,15 @@
 (define-module (gloa db utils)
   #:use-module (gloa db)
+  #:use-module (gloa db search)
   #:use-module (gloa article)
   #:export (article-present?
             vector->article))
 
 (define (article-present? db-path article)
   "Determine if ARTICLE is already present in the database at DB-PATH."
-  (let ((serialized-article-authors (serialize-article-authors
-                                     (article-authors article))))
-    (not (equal? 0
-                 (length
-                  (with-db db-path
-                    (query* "SELECT * FROM documents WHERE title = :title AND authors = :authors"
-                            (title (article-title article))
-                            (authors serialized-article-authors))))))))
+  (not (equal? 0
+               (length
+                (find-article db-path article)))))
 
 (define (vector->article vec)
   "Convert a vector representing a row from the returned SQLite query to an
