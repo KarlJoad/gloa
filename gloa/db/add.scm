fc9884edec0f36fa8fa18a7fdbb4aff48c836c6f
Karl Hallsby <karl@hallsby.com>
2023-06-11 07:49:34 -0500

    Insert new articles into the database if article not already present

--- gloa/db/add.scm
+++ gloa/db/add.scm
@@ -1,5 +1,6 @@
 (define-module (gloa db add)
   #:use-module (gloa db)
+  #:use-module (gloa db utils)
   #:use-module (gloa article)
   #:export (add-to-db))
 
@@ -8,8 +9,15 @@
 (define (add-to-db db-path article-info article-pdf-path)
   "Add ARTICLE-INFO metadata to the database at DB-PATH, noting the article PDF's
 path."
-  (let ((serialized-article-authors (string-join (article-authors article-info) " and ")))
-    (with-db db-path
-      (query* "INSERT INTO documents (title, authors) VALUES(:title, :authors)"
-              (title (article-title article-info))
-              (authors serialized-article-authors)))))
+  (let ((serialized-article-authors (serialize-article-authors
+                                     (article-authors article-info))))
+    (if (article-present? db-path article-info)
+        ;; FIXME: Convert these formats into logging statements
+        (format #t "~s already present in database. Not adding!~%"
+                (article-title article-info))
+        (begin
+          (format #t "~s not in database. Adding!~%" (article-title article-info))
+          (with-db db-path
+            (query* "INSERT INTO documents (title, authors) VALUES(:title, :authors)"
+                    (title (article-title article-info))
+                    (authors serialized-article-authors)))))))
