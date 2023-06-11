5cfcdb2e03e03cd1a02c26deb8805066588b7e12
Karl Hallsby <karl@hallsby.com>
2023-06-11 07:48:19 -0500

    Create conversion function for SQL row (vector) to article object

--- gloa/db/utils.scm
+++ gloa/db/utils.scm
@@ -1,7 +1,8 @@
 (define-module (gloa db utils)
   #:use-module (gloa db)
   #:use-module (gloa article)
-  #:export (article-present?))
+  #:export (article-present?
+            vector->article))
 
 (define (article-present? db-path article)
   "Determine if ARTICLE is already present in the database at DB-PATH."
@@ -13,3 +14,11 @@
                     (query* "SELECT * FROM documents WHERE title = :title AND authors = :authors"
                             (title (article-title article))
                             (authors serialized-article-authors))))))))
+
+(define (vector->article vec)
+  "Convert a vector representing a row from the returned SQLite query to an
+article object."
+  (let ((article-id (vector-ref vec 0)) ;; Drop the ID of the entry
+        (article-title (vector-ref vec 1))
+        (authors-list (deserialize-article-authors (vector-ref vec 2))))
+    (make-article article-title authors-list)))
