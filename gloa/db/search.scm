f2c7b929cff5f041e9f436d84d0179326b8a3545
Karl Hallsby <karl@hallsby.com>
2023-06-14 10:02:09 -0500

    Rewrite search to search database for article requested by title
    fix search

--- gloa/db/search.scm
+++ gloa/db/search.scm
@@ -2,12 +2,20 @@
   #:use-module (gloa db)
   #:use-module (gloa db utils)
   #:use-module (gloa article)
-  #:export (find-article))
+  #:export (find-article-by-title))
 
-(define (find-article db-path article)
+;; FIXME: Harden these functions and
+(define (find-article-by-title db-path article)
   "Attempt to find ARTICLE. If the article does not exist, return #f."
   (with-db db-path
-    (query* (lambda (entry) (vector->article entry))
-            "SELECT * FROM documents WHERE title = :title AND authors = :authors"
-            (title (article-title article))
-            (authors (serialize-article-authors article)))))
+    (let* ((title (article-title article))
+           (doc-id (list-ref
+                    (query* to-id "SELECT * FROM documents WHERE title = :title"
+                            (title title))
+                    0))
+           (author-list
+            (query* (lambda (info) (vector-ref info 0))
+                    "SELECT name FROM authors WHERE id IN
+(SELECT author_id FROM documents_authors_link WHERE document_id = :docid)"
+                    (docid doc-id))))
+      (make-article title author-list))))
