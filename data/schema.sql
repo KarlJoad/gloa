39fa3bed73fb6761853f437ae721defddde234fa
Karl Hallsby <karl@hallsby.com>
2023-06-14 03:45:11 -0500

    Drop the schema tables if they already exist
    We only use the schema when we first create the database. Thus, dropping
    tables that should not exist anyways essentially becomes a no-op. If the
    tables _did_ exist for some reason, then this will make sure we start with a
    clean slate.

--- data/schema.sql
+++ data/schema.sql
@@ -1,17 +1,20 @@
 PRAGMA foreign_keys = ON;
 
+DROP TABLE IF EXISTS documents;
 CREATE TABLE IF NOT EXISTS documents (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     title TEXT NOT NULL,
     authors TEXT NOT NULL
 );
 
+DROP TABLE IF EXISTS authors;
 CREATE TABLE IF NOT EXISTS authors (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
     name TEXT NOT NULL COLLATE NOCASE,
     UNIQUE(name)
 );
 
+DROP TABLE IF EXISTS documents_authors_link;
 CREATE TABLE documents_authors_link (
     id INTEGER PRIMARY KEY NOT NULL,
     document INTEGER NOT NULL,
