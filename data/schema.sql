10787ef6ba26f843e96cc3a4d728d2058d86f0c4
Karl Hallsby <karl@hallsby.com>
2023-06-14 09:16:46 -0500

    Remove authors from documents table's columns

--- data/schema.sql
+++ data/schema.sql
@@ -3,8 +3,7 @@ PRAGMA foreign_keys = ON;
 DROP TABLE IF EXISTS documents;
 CREATE TABLE IF NOT EXISTS documents (
     id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    title TEXT NOT NULL,
-    authors TEXT NOT NULL
+    title TEXT NOT NULL
 );
 
 DROP TABLE IF EXISTS authors;
