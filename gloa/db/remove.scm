31deef4fed34b58d6deb3a2c1efb4e33a3f28cf2
Karl Hallsby <karl@hallsby.com>
2023-06-15 02:57:30 -0500

    Remove a document from the DB, potentially removing authors
    Removing a document involves removing:
      1. The link between document & authors
      2. The document itself
      3. If no authors have contributed anything else, i.e. orphaned, those
         authors are removed too.

--- gloa/db/remove.scm
+++ gloa/db/remove.scm
@@ -4,9 +4,28 @@
   #:use-module (gloa article)
   #:export (remove-from-db))
 
+;; FIXME: If something does not exist when trying to remove, this whole thing
+;; fails with an "unexpected value ()" error.
 (define (remove-from-db db-path article)
   "Remove the ARTICLE from the database at DB-PATH."
   (with-db db-path
-    (query* "DELETE FROM documents WHERE title = :title AND authors = :authors"
-            (title (article-title article))
-            (authors (serialize-article-authors article)))))
+    (let ((doc-id (list-ref (query* to-id "SELECT id FROM documents WHERE title = :title"
+                                    (title (article-title article)))
+                            0))
+          (auth-ids (query* to-id
+                            (format #f "SELECT id from authors WHERE name IN ~a"
+                                    (list->sql (article-authors article))))))
+      ;; Remove the links in the authors-documents link table
+      (for-each (lambda (auth-id)
+                  (with-db db-path
+                    (query* "DELETE FROM documents_authors_link WHERE document_id = :docid AND author_id = :authid"
+                            (docid doc-id)
+                            (authid auth-id))
+                    ;; Remove any orphaned authors
+                    (unless (object-present? db-path "documents_authors_link" "author_id" auth-id)
+                      (with-db db-path (query* "DELETE FROM authors WHERE id = :authid"
+                                               (authid auth-id))))))
+                auth-ids)
+      ;; Remove the document itself
+      (with-db db-path (query* "DELETE FROM documents WHERE title = :title"
+                               (title (article-title article)))))))
