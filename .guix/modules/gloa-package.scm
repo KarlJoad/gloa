d854d1b7d3490b9da059a24831a9cb96fc37abc0
Karl Hallsby <karl@hallsby.com>
2023-08-02 13:56:19 -0500

    Allow building Gloa as a Guix channel
    From Guix blog post.
    https://guix.gnu.org/en/blog/2023/from-development-environments-to-continuous-integrationthe-ultimate-guide-to-software-development-with-guix/

---
+++ .guix/modules/gloa-package.scm
@@ -0,0 +1,73 @@
+;;; Gloa --- Academic article management with GNU Guile
+;;; Copyright © 2023 Karl Hallsby <karl@hallsby.com>
+;;;
+;;; This file is part of Gloa.
+;;;
+;;; Gloa is free software; you can redistribute it and/or modify it
+;;; under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; Gloa is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with Gloa.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; GNU Guix development package.  To build and install, run:
+;;
+;;   guix package -f guix.scm
+;;
+;; To use as the basis for a development environment, run:
+;;
+;;   guix shell -D -f guix.scm
+;;
+;;; Code:
+
+(define-module (gloa-package)
+  #:use-module (guix packages)
+  #:use-module (guix licenses)
+  #:use-module (guix utils)
+  #:use-module (guix gexp)
+  #:use-module (guix git-download)
+  #:use-module (guix build-system gnu)
+  #:use-module (gnu packages)
+  #:use-module (gnu packages autotools)
+  #:use-module (gnu packages guile)
+  #:use-module (gnu packages guile-xyz)
+  #:use-module (gnu packages pkg-config)
+  #:use-module (gnu packages texinfo)
+  #:use-module (gnu packages sqlite)
+  #:use-module (gnu packages guile))
+
+(define vcs-file?
+  ;; Return true if the given file is under version control.
+  (or (git-predicate (string-append (current-source-directory) "/../.."))
+      (const #t)))                                ;not in a Git checkout
+
+(define-public gloa
+  (package
+    (name "gloa")
+    (version "0.1")
+    (source (local-file "../.." "gloa-checkout"
+                        #:recursive? #t
+                        #:select? vcs-file?))
+    (build-system gnu-build-system)
+    (arguments
+     '(#:phases
+       (modify-phases %standard-phases
+         (add-after 'unpack 'bootstrap
+           (lambda _ (zero? (system* "sh" "bootstrap")))))))
+    (native-inputs (list autoconf automake pkg-config texinfo))
+    (inputs (list guile-3.0 sqlite guile-sqlite3))
+    (synopsis "Academic article management")
+    (description "Gloa is an academic article manage program written
+in Guile Scheme.")
+    (home-page "http://github.com/KarlJoad/gloa")
+    (license gpl3+)))
+
+gloa
