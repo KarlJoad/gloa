3ca94f8e0abe46a4af427f1558bba62ee399f1c4
Karl Hallsby <karl@hallsby.com>
2023-09-15 15:24:18 -0500

    Raise exception when searching for nonexistent title

--- gloa/db/search.scm
+++ gloa/db/search.scm
@@ -7,14 +7,17 @@
 ;; FIXME: Harden these functions and
 (define (find-article-by-title db-path title)
   "Attempt to find ARTICLE. If the article does not exist, return #f."
-  (with-db db-path
-    (let* ((doc-id (list-ref
-                    (query* to-id "SELECT * FROM documents WHERE title = :title"
-                            (title title))
-                    0))
-           (author-list
-            (query* (lambda (info) (vector-ref info 0))
-                    "SELECT name FROM authors WHERE id IN
+  (with-exception-handler
+      (lambda (exn) (raise-exception (make-no-sql-match-exception)))
+    (lambda ()
+      (with-db db-path
+        (let* ((doc-id (list-ref
+                        (query* to-id "SELECT * FROM documents WHERE title = :title"
+                                (title title))
+                        0))
+               (author-list
+                (query* (lambda (info) (vector-ref info 0))
+                        "SELECT name FROM authors WHERE id IN
 (SELECT author_id FROM documents_authors_link WHERE document_id = :docid)"
-                    (docid doc-id))))
-      (make-article title author-list))))
+                        (docid doc-id))))
+          (make-article title author-list))))))
