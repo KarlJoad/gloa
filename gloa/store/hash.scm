45923be62e66032e2a02c0504eaa5e25c31ab41b
Karl Hallsby <karl@hallsby.com>
2023-10-28 23:33:23 -0500

    Add function to hash a document's contents

---
+++ gloa/store/hash.scm
@@ -0,0 +1,11 @@
+(define-module (gloa store hash)
+  #:use-module (gloa config)
+  #:use-module (gcrypt hash)
+  #:use-module (rnrs bytevectors)
+  #:use-module (gloa article)
+  #:use-module (basexx base32) ;; bytevector->base32-string
+  #:export (hash-document))
+
+(define (hash-document file-path)
+  "Return the hash for the document at FILE-PATH."
+  (bytevector->base32-string (file-sha256 file-path)))
