ed70962aa9847d12e6d90c55a247f3cb57cfdfe7
Karl Hallsby <karl@hallsby.com>
2023-10-28 23:33:37 -0500

    Add module to make the base32 file name of a document

---
+++ gloa/store/entry.scm
@@ -0,0 +1,23 @@
+(define-module (gloa store entry)
+  #:use-module (gloa config)
+  #:use-module (gloa store hash)
+  #:use-module (gloa article)
+  #:export (base32-file-name))
+
+;; On Linux, most file systems have a maximum path length of 255 characters.
+;; The hash we get is 52 characters, and if we assume the average user's storage
+;; directory is ~50 characters, we still have ~150 to work with, so we should be
+;; fine.
+(define (base32-file-name article article-file-path)
+  (let* ((title (article-title article))
+         (title-down (string-downcase title))
+         (fixed-title (string-join
+                       (string-split title-down #\SPACE)
+                       "-"
+                       'infix))
+         (extension (substring article-file-path
+                               (string-index-right article-file-path #\.))))
+    (string-append (hash-document article-file-path)
+                   "-"
+                   fixed-title
+                   extension)))
