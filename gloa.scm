e9dcd4bfd55a94540554f2b87dd2c75f38bd3bc1
Karl Hallsby <karl@hallsby.com>
2023-10-28 23:45:49 -0500

    Refactor configuration out to dedicated module

--- gloa.scm
+++ gloa.scm
@@ -1,48 +1,15 @@
 (define-module (gloa)
+  #:use-module (gloa config)
   #:use-module (gloa hello)
   #:use-module (system repl repl)
   #:declarative? #f
   #:export (gloa-main))
-
-(define guile-user-config
-  ;; The user's personal Guile REPL configuration.
-  (and=> (getenv "HOME")
-         (lambda (home)
-           (string-append home "/.guile"))))
 
 (define (set-user-module)
   (when (and ;; (not (assoc-ref opts 'ignore-dot-guile?)) ;; TODO: CLI flag for ignoring .guile
              guile-user-config
              (file-exists? guile-user-config))
     (load guile-user-config)))
-
-(define (%default-db-location)
-  "The default location of Gloa's tracking database."
-  (string-append
-   (or (getenv "XDG_CACHE_HOME")
-       (format #f "~a/.cache" (getenv "HOME")))
-   "/gloa/gloa.sqlite"))
-
-(define (%default-storage-directory)
-  "The default location of Gloa's tracked documents."
-  (string-append
-   (or (getenv "XDG_DATA_HOME")
-       (format #f "~a/.local" (getenv "HOME")))
-   "/gloa/"))
-
-(define (%default-user-config-location)
-  "Location of user's configuration of Gloa.
-Searches for @code{$XDG_CONFIG_HOME/gloa/init.scm} first, if that is not found,
-Gloa falls back to @code{$HOME/.gloa.d/init.scm}, and if that fails,
-@code{$HOME/.gloa.scm} is searched for.
-If none of those are found, no configuration changes to Gloa occur."
-  (let ((config-dot-d (format #f "~a/.gloa.d" (getenv "HOME")))
-        (dot-config (format #f "~a/.gloa.scm" (getenv "HOME"))))
-    (cond ((getenv "XDG_CONFIG_HOME")
-           (string-append (getenv "XDG_CONFIG_HOME") "/gloa/init.scm"))
-          ((and (file-exists? config-dot-d) (file-is-directory? config-dot-d))
-           (format #f "~a/init.scm" config-dot-d))
-          (#t dot-config))))
 
 (define* (gloa-main arg0 . args)
   ;; Add gloa site directory to Guile's load path so that user's can
