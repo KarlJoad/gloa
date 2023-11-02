23ec60b823192b0b2dedfdb91cbcbeba21311ad8
Karl Hallsby <karl@hallsby.com>
2023-11-01 23:56:49 -0500

    Add ops module to pull together store & db operations to single command
    It is easier (and more correct) to only expose the add-document procedure
    rather than both the add-to-store and add-to-db procedures. Exposing those two
    lower-level APIs allows us to reach an inconsistent state between the store
    and the database.
    
    In the long run, we should have a way to verify and perhaps correct either the
    store or database when they become inconsistent with each other. This would
    function similarly to Guix's "guix gc --verify" command.

---
+++ gloa/ops/add.scm
@@ -0,0 +1,12 @@
+(define-module (gloa ops add)
+  #:use-module (gloa config)
+  #:use-module (gloa db add)
+  #:use-module (gloa store add)
+  #:export (add-document))
+
+(define (add-document article article-path)
+  "Register ARTICLE with the database and copy the document from ARTICLE-PATH to
+the document store."
+  (begin
+    (add-to-db (%default-db-location) article article-path)
+    (add-to-store article article-path)))
