faa37f6e800c8184f2f7d573f78bd189ed4be001
Karl Hallsby <karl@hallsby.com>
2023-11-02 23:05:26 -0500

    Move general Gloa utilities to gloa base directory

--- gloa/store/add.scm
+++ gloa/store/add.scm
@@ -1,10 +1,12 @@
 (define-module (gloa store add)
   #:use-module (gloa config)
   #:use-module (gloa store entry)
-  #:use-module (gloa store utils)
+  #:use-module (gloa utils)
   #:export (add-to-store))
 
 (define (add-to-store article article-file-path)
+  "Add ARTICLE to the document store and copying ARTICLE-FILE-PATH to the
+document store."
   (let* ((result-path (base32-file-name article article-file-path))
          (out (string-append (%default-storage-directory) result-path)))
     (mkdir-p (%default-storage-directory))
